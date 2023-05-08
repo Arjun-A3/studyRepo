4d2b9c5425f1808a4ff1de2880c10295c6236311
Arjun-A3 <arjun43250@gmail.com>
2023-05-08 21:48:14 +0530

    signup page created

--- clientSide/my-app/src/App.js
+++ clientSide/my-app/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import login from './login';
+import signup from './signup'
 
 function App() {
   return (
-   <div>{login()}</div> 
+   <div>{signup()}</div> 
   );
 }
 
