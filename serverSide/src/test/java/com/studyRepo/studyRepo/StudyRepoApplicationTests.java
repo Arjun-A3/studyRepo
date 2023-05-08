b936ca07533fc7a9d206626b6ba7ef7e9d626aa1
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-05-08 11:24:51 +0530

    added springboot file in serverSide file

---
+++ serverSide/src/test/java/com/studyRepo/studyRepo/StudyRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studyRepo.studyRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
