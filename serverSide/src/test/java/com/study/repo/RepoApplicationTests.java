7e86013261b5fb57f2e5174aa06afc084655eb85
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-05-10 09:37:58 +0530

    created a spring project with maven

---
+++ serverSide/src/test/java/com/study/repo/RepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
