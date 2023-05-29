8c1977c6ebb6da374dcd4753d16e9fdc731c2037
Arjun <110415386+Arjun-A3@users.noreply.github.com>
2023-05-29 15:37:15 +0530

    changed

--- frontend/src/Login/Login.js
+++ frontend/src/Login/Login.js
@@ -15,7 +15,10 @@ export const PrivateRoutes = () => {
      else if (isLoggedIn == 'Fac') 
         return <Navigate to='/Upload'/>
      else 
-       return <Outlet/>
+        return <Outlet/>
+
+    
+
 }
 
 
@@ -68,9 +71,9 @@ export function Login() {
         isLoggedIn = "Fac";
         console.log(details);
         axios.post(API_URL, details).then((Response) => {
-            if (Response) 
+            if (Response) {      
                 window.location = "/upload";
-             else {
+            } else {
                 alert("failed")
             }
         }).catch((e) => {
@@ -100,7 +103,7 @@ export function Login() {
                                             <p id="fontcolor">Please Login To Your Account</p>
 
                                             <div className="form-outline mb-4">
-                                                <input type="email" id="email" name="email" className="form-control" placeholder="Phone number or email address"
+                                                <input type="email" id="email" name="email" className="form-control" placeholder="email address"
                                                     onChange={handleChange}/>
 
                                             </div>
@@ -112,12 +115,12 @@ export function Login() {
                                             </div>
 
                                             <div className="text-center pt-1 mb-5 pb-1">
-                                                <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button" name="desc" value="Faculty"
-                                                    onClick={()=>{handleChange;handleInputt}}>Log
-                                                                                  in as Faculty</button>
-                                                <button id="b2" className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button" name="desc" value="Student"
-                                                    onClick={()=>{handleChange;handleInputt}}>Log
-                                                                                  in as Student</button>
+                                                <button className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button"
+                                                    onClick={handleInputt}>Log
+                                                                                                                                                                                                  in as Faculty</button>
+                                                <button id="b2" className="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button"
+                                                    onClick={handleInputst}>Log
+                                                                                                                                                                                                  in as Student</button>
                                                 {/* {()=>{navigate("/Stu")}} */} </div>
 
                                             <div className="d-flex align-items-center justify-content-center pb-4">
@@ -137,7 +140,7 @@ export function Login() {
                                 <div className="col-lg-6 d-flex align-items-center gradient-custom-2">
                                     <div className="text-black px-3 py-4 p-md-5 mx-md-4">
                                         <h4 className="mb-4">What We Are?!</h4>
-                                        <p className="small mb-0">Our project aims to develop a web-based platform that facilitates the sharing of learning resources between teachers and students. The platform will consist of two login pages, one for teachers and another for students. Teachers can log in to the platform and upload files such as lecture notes, presentations, and assignments. Students, on the other hand, can log in to the platform and view or download these files for their exam preparation. The files uploaded by teachers will be stored on a server and organized by course and topic for easy access by students. The platform will also include a search function that allows students to search for files by keyword.</p>
+                                        <p className="small mb-0">Our project aims to develop a web-based platform that facilitates the sharing of learning resources between teachers and students. The platform will consist of two login pages, one for teachers and another for students. Teachers can log in to the platform and upload files such as lecture notes, presentations, and assignments. Students, on the other hand, can log in to the platform and  download these files for their exam preparation. The files uploaded by teachers will be stored on a server and organized by course and topic for easy access by students. The robustness, efficiency, and security of this website are enhanced through the utilization of React.js, Spring Boot, and MongoDB in its development.</p>
                                     </div>
                                 </div>
                             </div>
